7fd932e706dff49e54c7d4854ff9bc1dbccf03cb
missicipi <professorulissesm@gmail.com>
2020-07-12 17:33:16 -0300

    Primeiro commit

---
+++ src/main/java/com/sistemawebservice/br/SistemaWebApplication.java
@@ -0,0 +1,13 @@
+package com.sistemawebservice.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaWebApplication.class, args);
+	}
+
+}
