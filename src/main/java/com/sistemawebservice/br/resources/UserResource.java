356ffc22e69f8b5fca765dd145b7bccbb1834255
missicipi <professorulissesm@gmail.com>
2020-07-12 18:00:14 -0300

    User entity e Resource ok

---
+++ src/main/java/com/sistemawebservice/br/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.sistemawebservice.br.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sistemawebservice.br.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "mama", "maria@gmail.com", "12345");
+		
+		return ResponseEntity.ok().body(u);
+	}
+
+	
+}
